7f3f030a75f001a610f4734ba2a6e4e2e606cbd8
code12570-bit <2049230232@qq.com>
2025-01-28 09:42:31 +0800

    new file ant-mall

---
+++ ant-mall/src/test/java/com/sanxiaxing/antmall/AntMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanxiaxing.antmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
