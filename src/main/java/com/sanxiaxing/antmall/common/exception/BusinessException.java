1d9af1567b2f64b9349d0217528eaf926b69c75d
code12570-bit <2049230232@qq.com>
2025-02-13 20:03:09 +0800

    添加公共组件

---
+++ src/main/java/com/sanxiaxing/antmall/common/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.sanxiaxing.antmall.common.exception;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException() {
+        super();
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
